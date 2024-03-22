6d1f1e1f1e43d15792aa77bfc1353e539718ddc0
Geoffrey Weal <geoffreyweal@Geoffreys-iMac.local>
2024-03-22 21:07:27 +0900

    updated repository for the ORCA Coalesce Reactants Procedure

--- pushing_to_github.sh
+++ pushing_to_github.sh
@@ -1,13 +1,13 @@
 # For commiting to github for the first time
-rm -rf .git
-git init
+#rm -rf .git
+#git init
 # upload to github
 # upload to github
 git add .
 git commit -m 'updated repository for the ORCA Coalesce Reactants Procedure'
 # For commiting to github for the first time
-git branch -M main
-git remote add origin git@github.com:geoffreyweal/ORCA_Coalesce_Reactants_Procedure.git
-git push -uf origin main
+#git branch -M main
+#git remote add origin git@github.com:geoffreyweal/ORCA_Coalesce_Reactants_Procedure.git
+#git push -uf origin main
 # push your new commit:
 git push -u origin main
